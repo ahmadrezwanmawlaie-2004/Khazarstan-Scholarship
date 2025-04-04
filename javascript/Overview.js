61f3effecce1373d41c6e654be6f0b7f7dcf761f
Ahmad Rezwan Mawlaie <rezwanmawlaei1@gmail.com>
2025-04-04 21:55:05 +0430

    Javascript file is created, several changes add to the desgin of overview: including: bg of experience to light and scrollpy to light

---
+++ javascript/Overview.js
@@ -0,0 +1,8 @@
+const eventDisplay= ()=> {
+    const toggle = document.getElementById("event");
+    toggle.toggleAttribute("hidden");
+}
+const degreeDisplay = () => {
+    const toggle = document.getElementById("degree");
+    toggle.toggleAttribute("hidden");
+}
