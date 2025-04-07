b4d2512431ccd73c53092b2a63a2defe892aefba
Ahmad Rezwan Mawlaie <rezwanmawlaei1@gmail.com>
2025-04-07 16:11:47 +0430

    The dashboard file took six university with its ES6 toggle attribute

--- javascript/Dashboard.js
+++ javascript/Dashboard.js
@@ -2,3 +2,23 @@ let universityFirstHidden = () => {
     let toggle = document.getElementById("FirstUniversity");
     toggle.toggleAttribute("hidden");
 }
+let universitySecondHidden = () => {
+    let toggle = document.getElementById("SecondUniversity");
+    toggle.toggleAttribute("hidden");
+}
+let universityThirdHidden = () => {
+    let toggle = document.getElementById("ThirdUniversity");
+    toggle.toggleAttribute("hidden");
+}
+let universityFourthHidden = () => {
+    let toggle = document.getElementById("FourthUniversity");
+    toggle.toggleAttribute("hidden");
+}
+let universityFifthHidden = () => {
+    let toggle = document.getElementById("FifthUniversity");
+    toggle.toggleAttribute("hidden");
+}
+let universitySixthHidden = () => {
+    let toggle = document.getElementById("SixthUniversity");
+    toggle.toggleAttribute("hidden");
+}
