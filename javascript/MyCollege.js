1e2c1a56d0cdc2cc315f89194c732f47c2a2e4eb
Ahmad Rezwan Mawlaie <rezwanmawlaei1@gmail.com>
2025-04-09 17:53:36 +0430

    The changes took in mycollege section both html and css

---
+++ javascript/MyCollege.js
@@ -0,0 +1,14 @@
+const buttons = document.querySelectorAll(".buttons");
+const detailCollege = document.querySelectorAll(".colleges");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const targetId = button.getAttribute("data-target");
+        detailCollege.forEach(detail => {
+            detail.style.display = "none";
+        });
+        document.getElementById("myCollege").style.display = "none";
+        const targetDetail = document.getElementById(targetId);
+        targetDetail.style.display = 'block';
+    });
+});
