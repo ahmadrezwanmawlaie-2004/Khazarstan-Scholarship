f732d03556d3e083aec7d4149d872d9ec8807b69
Ahmad Rezwan Mawlaie <rezwanmawlaei1@gmail.com>
2025-04-26 17:08:27 +0430

    New Changes took place

--- javascript/SearchingModule.js
+++ javascript/SearchingModule.js
@@ -1,14 +1,35 @@
-const searchValue = document.getElementById("searchBar");
-searchValue.addEventListener("input", function () {
-    const searchString = searchValue.value.toLowerCase();
-    const searchResults = document.querySelectorAll(".colleges");
-    searchResults.forEach((college) => {
-        const collegeName = college.querySelector(".college-name").textContent.toLowerCase();
-        if (collegeName.includes(searchString)) {
-            college.style.display = "block";
-        } else {
-            college.style.display = "none";
-        }
-        
+// This module handles the searching functionality for the college search application;
+let dashboardCollegeSearch = () => {
+    const searchValue = document.getElementById("searchBar");
+    searchValue.addEventListener("input", function () {
+        const searchString = searchValue.value.toLowerCase();
+        const searchResults = document.querySelectorAll(".colleges");
+        searchResults.forEach((college) => {
+            const collegeName = college.querySelector(".college-name").textContent.toLowerCase();
+            if (collegeName.includes(searchString)) {
+                college.style.display = "block";
+            } else {
+                college.style.display = "none";
+            }
+
+        });
     });
-});
+};
+dashboardCollegeSearch();
+let collegeCollegeSearch = () => {
+    const searchValue = document.getElementById("searchBarCollegeSearch");
+    searchValue.addEventListener("input", function () {
+        const searchString = searchValue.value.toLowerCase();
+        const searchResults = document.querySelectorAll(".colleges");
+        searchResults.forEach((college) => {
+            const collegeName = college.querySelector(".college-name").textContent.toLowerCase();
+            if (collegeName.includes(searchString)) {
+                college.style.display = "block";
+            } else {
+                college.style.display = "none";
+            }
+
+        });
+    });
+};
+collegeCollegeSearch();
