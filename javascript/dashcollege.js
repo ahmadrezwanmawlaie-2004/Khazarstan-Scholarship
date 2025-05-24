640ba37c7292ae016cb48d4e478655fd6c2e03f3
Ahmad Rezwan Mawlaie <rezwanmawlaei1@gmail.com>
2025-05-24 18:04:40 +0430

    New change in connecting pages

--- javascript/dashcollege.js
+++ javascript/dashcollege.js
@@ -7,10 +7,38 @@
     
 // });
 const userNameJS = document.getElementById("userName");
-userNameJS.innerHTML = localStorage.getItem(`name:1`)
+userNameJS.innerHTML = localStorage.getItem(`name:1`);
 
 const userEmailJS = document.getElementById("userEmail");
 userEmailJS.innerHTML = localStorage.getItem("email:1");
 
 const userIdJS = document.getElementById("userId");
 userIdJS.innerHTML = localStorage.getItem("khazarID:1");
+
+
+const khazarID = Math.floor(Math.random() * 1000000);
+const transfterStudentInformationJSON = localStorage.getItem("transferStudentInformation");
+const transfterStudentInformation = JSON.parse(transfterStudentInformationJSON);
+userNameJS.innerHTML = transfterStudentInformation.nameFirst;
+userEmailJS.innerHTML = transfterStudentInformation.emailAddress;
+userIdJS.innerHTML = khazarID;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- javascript/js.js
+++ javascript/js.js
@@ -168,4 +168,57 @@ locationSubmissionJS.addEventListener("click", () => {
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
+const transferAccountCreation = document.getElementById("transferButtonAccount");
+const inputValues = document.querySelectorAll(".transferValues");
+// Now accessing each txts, radios, checkbox
+const txt1 = document.getElementById("fName");
+const txt2 = document.getElementById("lName");
+const txt3 = document.getElementById("sName");
+const txt4 = document.getElementById("tCemail");
+const txt5 = document.getElementById("temail");
+const txt6 = document.getElementById("preferNumber");
+const txt7 = document.getElementById("alternatePhone");
+const txt8 = document.getElementById("userName");
+const txt9 = document.getElementById("passwordT");
+const txt10 = document.getElementById("passwordRT");
+const pattern2 = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[\d]).{8,32}$/;
+const pattern3 = /^\+?[0-9]+$/;
+const alertAccountCreationTransferStudent = document.getElementById("alertAccountCreation");
 
+transferAccountCreation.addEventListener("click", () => {
+    for (let i = 0; i < inputValues.length; i++) {
+        if (inputValues[i].value === "") {
+            alert("Please fill all the fields! 🤔");
+            break;
+        };
+    };
+    if (txt4.value !== txt5.value) {
+        alert("The email addresses do not match! ❗");
+        return;
+
+    } else if (pattern3.test(txt6.value) === false && pattern3.test(txt7.value) === false) {
+        alert("Please write a valid phone number! ❗");
+        return;
+
+    } else if (pattern2.test(txt9.value) === false) {
+        alert("The password must contain letters, numbers and special characters! ❗");
+        return;
+
+    } else if (txt9.value !== txt10.value) {
+        alert("The passwords do not match! ❗");
+        return;
+    } else {
+        let transferStudentInfo = {
+            nameFirst: `${txt1.value}`, nameLast: `${txt2.value}`, suffix: `${txt3.value}`, emailAddress: `${txt5.value}`, phoneNumber: `${txt6.value}`, alternativePhoneNumber: `${txt7.value}`, userName: `${txt8.value}`, userPassword: `${txt9.value}`
+        };
+        const tranferInformation = JSON.stringify(transferStudentInfo);
+        localStorage.setItem("transferStudentInformation", tranferInformation);
+        alertAccountCreationTransferStudent.style.display = "block";
+        setTimeout(() => {
+            window.open("Dashboard.html", "_self");
+        }, 1000);
+    }
+    for (let i = 0; i < inputValues.length; i++) {
+        inputValues[i].value = "";
+    }
+})
